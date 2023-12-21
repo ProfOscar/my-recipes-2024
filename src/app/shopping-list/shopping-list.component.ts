0fb201f41682d917a9b37843949179af49294c0c
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2023-12-21 08:49:42 +0100

    shopping-list, shopping-edit, recipe-detail, recipe-item components prepared

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IngredientModel } from '../models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
-
+  ingredients: IngredientModel[] = [
+    new IngredientModel("Apple", 5),
+    new IngredientModel("Tomato", 10)
+  ];
 }
