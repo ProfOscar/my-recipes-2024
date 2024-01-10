3d3736a2d1e0bed602c0f2e6645b272833f8ef3a
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2024-01-10 11:40:53 +0100

    Shopping list managing

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { IngredientModel } from 'src/app/models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent {
+  @Output() ingredientAdded = new EventEmitter<IngredientModel>();
+
+  ingredientName: string = "";
+  ingredientAmount: number = 0;
+
+  onAddIngredient() {
+    if (this.ingredientAmount <= 0) {
+      alert("Amount should be positive");
+    } else {
+      const newIngredient: IngredientModel = new IngredientModel(this.ingredientName, this.ingredientAmount);
+      this.ingredientAdded.emit(newIngredient);
+      this.onClear();
+    }
+  }
+
+  onClear() {
+    this.ingredientName = "";
+    this.ingredientAmount = 0;
+  }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -11,4 +11,14 @@ export class ShoppingListComponent {
     new IngredientModel("Apple", 5),
     new IngredientModel("Tomato", 10)
   ];
+
+  onIngredientAdded(newIngredient: IngredientModel) {
+    let ingredientFound = this.ingredients.find(function (item) {
+      return item.name.toLowerCase() == newIngredient.name.toLowerCase();
+    })
+    if (!ingredientFound)
+      this.ingredients.push(newIngredient);
+    else
+      ingredientFound.amount += newIngredient.amount;
+  }
 }
