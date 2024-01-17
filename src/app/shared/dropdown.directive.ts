ad7516630c5c7a0af6ea7f2be6e2f90873292e45
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2024-01-17 12:57:58 +0100

    Close the dropdown menu when user clicks outside the button

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,11 +1,23 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, HostBinding, HostListener, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
+  
   @HostBinding('class.open') isOpen:boolean = false;
-  @HostListener('click') toggleOpen(event:Event){
-    this.isOpen = !this.isOpen;
+  
+  // @HostListener('click') toggleOpen(event:Event){
+  //   this.isOpen = !this.isOpen;
+  // };
+  
+  @HostListener('document:click', ['$event']) toggleOpen(event:Event){
+    if (this.myRef.nativeElement.contains(event.target))
+      this.isOpen = !this.isOpen;
+    else
+      this.isOpen = false;
   }
+
+  constructor(private myRef: ElementRef) { }
+
 }
