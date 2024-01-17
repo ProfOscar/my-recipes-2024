3083cdb1e5a8508be4864c80e34768e3be6f9419
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2024-01-17 12:46:52 +0100

    Added a directive to open/close the dropdown menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { FormsModule } from '@angular/forms';
 import { HighlightDirective } from './shared/highlight.directive';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { HighlightDirective } from './shared/highlight.directive';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    HighlightDirective
+    HighlightDirective,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
