450d51809d9f5fe8d34bb451de13a9ce6d1a6301
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2024-01-24 12:36:08 +0100

    Added DataStorageService to load the recipes from a web service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { IngredientModel } from 'src/app/models/ingredient.model';
 import { RecipeModel } from 'src/app/models/recipe.model';
+import { DataStorageService } from 'src/app/shared/data-storage.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,49 +10,27 @@ import { RecipeModel } from 'src/app/models/recipe.model';
 export class RecipeListComponent {
   @Output() recipeItemSelected = new EventEmitter<RecipeModel>();
 
-  recipes: RecipeModel[] = [
-    new RecipeModel(
-      "Spaghetti alla chitarra",
-      "Un particolare tipo di pasta che...",
-      "https://images.lacucinaitaliana.it/wp-content/uploads/2017/02/Maccheroni-alla-chitarra.jpg",
-      [new IngredientModel('Apple',
-        5), new IngredientModel('Tomato',
-          10)
-      ]),
-    new RecipeModel(
-      "Lasagne alla bolognese",
-      "Pasta calorica emiliana...",
-      "https://www.cucinare.it/uploads/wp-content/uploads/2015/05/pasta_alla_bolognese.webp",
-      [new IngredientModel('Apple',
-        6), new IngredientModel('Tomato',
-          11)
-      ]),
-    new RecipeModel(
-      "Gnocchi ai formaggi",
-      "ottimi soprattutto in montagna...",
-      "https://images.fidhouse.com/fidelitynews/wp-content/uploads/sites/6/2017/05/1495792337_13c855513b63705bb079c377ed01563d4eb12ccc-223759579.jpg?w=580",
-      [new IngredientModel('Apple',
-        7), new IngredientModel('Tomato',
-          12)
-      ]),
-    new RecipeModel(
-      "Tiramisu",
-      "con panna e mascarcope...",
-      "https://blog.giallozafferano.it/acasadimaria/wp-content/uploads/2019/06/tiramisu-sav.png",
-      [new IngredientModel('Apple',
-        8), new IngredientModel('Tomato',
-          13)
-      ])
-  ];
+  recipes: RecipeModel[] = [];
 
-  selectedRecipe: RecipeModel;
+  selectedRecipe?: RecipeModel;
 
-  constructor() {
-    this.selectedRecipe = this.recipes[0];
-  }
+  constructor(private dataStorageService: DataStorageService) { }
 
   ngOnInit() {
-    this.recipeItemSelected.emit(this.selectedRecipe);
+    this.dataStorageService.inviaRichiesta("get", "/recipes")?.subscribe({
+      "next": (data) => {
+        this.recipes = data;
+        this.selectedRecipe = this.recipes[0];
+        this.recipeItemSelected.emit(this.selectedRecipe);
+      },
+      "error": (error) => {
+        console.log(error);
+      }
+    });
+
+
+
+
   }
 
   onRecipeItemSelected(recipe: RecipeModel) {
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataStorageService {
+
+  private REST_API_SERVER = "http://localhost:3000/api";
+
+  constructor(private httpClient: HttpClient) { }
+
+  public inviaRichiesta(method: string, resource: string, params: any = {}): Observable<any> | undefined {
+    resource = this.REST_API_SERVER + resource;
+    switch (method.toLowerCase()) {
+      case "get":
+        return this.httpClient.get(resource, { "params": params });
+        break;
+      case "delete":
+        return this.httpClient.delete(resource, { "body": params });
+        break;
+      case "post":
+        return this.httpClient.post(resource, params);
+        break;
+      case "patch":
+        return this.httpClient.patch(resource, params);
+        break;
+      default:
+        return undefined;
+        break;
+    }
+  }
+}
